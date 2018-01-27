7de203a69a206a7276abd5801deb3ea6cd39b569
De Wet van Niekerk <dewert@gmail.com>
2018-01-27 15:20:28 -0800

    Revert "fixed merge conflit"
    This reverts commit 4313f0bca9b8abeb2afebfa9e52eda319214897a, reversing
    changes made to 74e74c4ac0154a6a64aa0838f08b41b85b824432.

--- MudServer/lib/connectionman/ConnectionContainer.cpp
+++ MudServer/lib/connectionman/ConnectionContainer.cpp
@@ -1,62 +1,70 @@
 #include "ConnectionContainer.h"
-ConnectionContainer::ConnectionContainer(): mProtocol(std::unique_ptr<MudProtocol>(new MudProtocol(512))) {}
 
 ConnectionContainer::ConnectionContainer(const Connection& c) {
-  mConnection = c;
-  mProtocol = std::unique_ptr<MudProtocol>(new MudProtocol(512));
-  isConnected = true;
+    m_connection = c;
+    
+    m_protocol = std::unique_ptr<Protocol>(new MudProtocol(512));
+    
+    isConnected = true;
+    
+    randid = rand();
+
+    //fix this to unique_ptrs
+    LoginHandler* loginHandle = new LoginHandler();
+
+    this->pushToStack(*loginHandle);
+
+    username = "";
+
+   
 }
 
-ConnectionContainer::ConnectionContainer(ConnectionContainer &&container): mConnection(container.mConnection), mProtocol(std::move(container.mProtocol)) {}
+void ConnectionContainer::pushToStack(Handler& handler) {
+  m_handlers.push(&handler); 
+  m_handlers.top()->welcome(this);
+}
 
-//receives messages from Server -> ConnectionManager->ConnectionContainer->Protocol
-void ConnectionContainer::receiveFromServer(std::string& str) {
-  // translated = mProtocol.receive(str);
-  try {
-    mProtocol->receive(str);
-  } catch (std::exception& e) {
+Handler& ConnectionContainer::getHandler() {
+  Handler* handler = m_handlers.top();
+  return *handler;
+}
+
+//receives messages from ConnectionManager
+void ConnectionContainer::receive(const std::string& str) {
+  std::string translated;
+
+  // translated = m_protocol.receive(str);
+  try{
+    translated = m_protocol->receive(str);
+  }catch(std::exception& e) {
     //kick user out
-    std::cout << e.what() << std::endl;
+    std::cout<<e.what()<<std::endl;
     isConnected = false;
     return;
   }
 
-  return;
-}
+  // std::cout<<randid<<" msg received: "<<translated<<std::endl;
 
-std::string ConnectionContainer::sendToGameManager() {
-  auto str = mProtocol->send(); 
-  return str;
-}
-
-void ConnectionContainer::receiveFromGameManager(std::string& str) {
-  std::cout<<"connection container received from game manager: "<<str<<std::endl;
+  //pass translated msg to handler
+  if(!translated.empty()) {
+   m_handlers.top()->handle(this,translated);
+  }
   
-  // translated = mProtocol.receive(str);
-  try {
-   mProtocol->receive(str);
-  } catch (std::exception& e) {
-    //kick user out
-    std::cout << e.what() << std::endl;
-    isConnected = false;
-    return;
-  }
-
   return;
 }
 
-std::string ConnectionContainer::sendToServer() {
-  return mProtocol->send();
-}
-
-bool ConnectionContainer::getIsConnected() const {
+bool ConnectionContainer::getIsConnected() {
   return isConnected;
 }
 
-Connection ConnectionContainer::getConnection() const {
-  return mConnection;
+Connection& ConnectionContainer::getConnection() {
+  return m_connection;
 }
 
-MudProtocol& ConnectionContainer::getProtocol() const {
-  return *mProtocol;
+std::string ConnectionContainer::getOutBuffer() {
+  return m_protocol->send();
+}
+
+void ConnectionContainer::sendToProtocol(const std::string& str) {
+  m_protocol->sendToBuffer(str);
 }
--- MudServer/lib/connectionman/ConnectionManager.cpp
+++ MudServer/lib/connectionman/ConnectionManager.cpp
@@ -1,144 +1,76 @@
 #include "ConnectionManager.h"
 
-ConnectionManager::ConnectionManager() {
-    m_list = new ConnectionList();
+ConnectionManager::ConnectionManager(ConnectionList* list): m_list(list) {
+    m_protocol = std::unique_ptr<Protocol>(new MudProtocol(1024));
 }
 
-/*checks each ConnectionContainers's isConnected state. If isConnected is false, then remove the container and
+/*checks each ConnectionContainers's isConnected state. If isConnected is false, then remove the container and 
 drop connection.*/
-void ConnectionManager::dropConnections() {
-    for (const auto& c : *m_list) {
+void ConnectionManager::dropConnection(Server& server) {
+    for(auto& c : (*m_list)) {
 
-        if ((*c).getIsConnected() == false) {
-            const auto& toBeRmved = (*c).getConnection();
-            m_list->erase(std::remove(m_list->begin(), m_list->end(), c));
+        if((*c).getIsConnected() == false) {
+            Connection toBeRmved = (*c).getConnection();
+            m_list->erase(std::remove(m_list->begin(), m_list->end(),c));
             server.disconnect(toBeRmved);
-        }
+        }   
     }
 }
 
 void ConnectionManager::addConnection(Connection c) {
-    auto ptr = std::make_unique<ConnectionContainer>(c);
-
+    auto ptr = std::unique_ptr<ConnectionContainer>(new ConnectionContainer(c));
     m_list->push_back(std::move(ptr));
 }
 
-/*pass clients messages to existing connection containers If client does not exist,
+/*pass clients messages to existing connection containers If client does not exist, 
 create new connection containers.
 */
-void ConnectionManager::rxFromServer(std::deque<Message> &incoming) {
-    for (auto& msg : incoming) {
+void ConnectionManager::passMessages(const std::deque<Message> &incoming) {
+    for(auto& msg: incoming) {
         auto conn = msg.connection;
         auto text = msg.text;
-        // std::cout << "msg from: " << msg.connection.id << " " << msg.text << std::endl;
+        std::cout<<"msg from: "<<msg.connection.id<<" "<<msg.text<<std::endl;
 
-        auto connContainerItr = std::find_if(m_list->begin(), m_list->end(), find_container(conn));
+        auto connContainer = std::find_if(m_list->begin(),m_list->end(),find_container(conn));
 
-        if (connContainerItr == m_list->end()) {
-            addConnection(conn);
+        if (connContainer == m_list->end()) {
+            ConnectionManager::addConnection(conn);
         }
 
-        connContainerItr = std::find_if(m_list->begin(), m_list->end(), find_container(conn));
-    
-        (*connContainerItr)->receiveFromServer(text);
+        connContainer = std::find_if(m_list->begin(), m_list->end(),find_container(conn));
+
+        (*connContainer)->receive(text);
     }
 }
 /*Polls the list of connection containers for any buffered messages waiting to be sent
 */
-std::deque<Message> ConnectionManager::sendToServer() {
+void ConnectionManager::sendMessages(Server& server) {
     std::deque<Message> messages;
 
-    for (const auto& container : *m_list) {
-        const auto& conn = container->getConnection();
-        const auto& toSend = container->sendToServer();
-
-        if (!toSend.empty()) {
-            Message m_msg = Message{conn, toSend};
+    for(auto& container : (*m_list)) {
+        Connection conn = container->getConnection();
+        std::string toSend = container->getOutBuffer();
+        
+        if(!toSend.empty()) {
+            Message m_msg = Message{conn,toSend};
             messages.push_back(m_msg);
         }
     }
 
     server.send(messages);
-
-    return messages;
 }
 
-//collect and pass msgs from protocols to the GameManager
-gameAndUserMsgs& ConnectionManager::send2GameManager() {
+void ConnectionManager::broadCast(Server& server, std::string& broadcast) {
+    std::deque<Message> messages;
     
-    for (const auto& container : *m_list) {
-        const auto& user_msg = container->sendToGameManager();
-        const auto& c = container->getConnection();
-        std::cout<<c.id<<": "<<user_msg<<std::endl;
+    auto res = m_protocol->broadcast(broadcast);
 
-        if (!user_msg.empty()) {
-            auto msg = std::make_unique<gameAndUserInterface>();
-            msg->text = user_msg;
-            msg->conn = c;
-            msgsToGameManager.push_back(std::move(msg));
-        }
+    for(auto& container : (*m_list)) {
+        Connection conn = container->getConnection();
+        Message m_msg = Message{conn,res};
+        messages.push_back(m_msg);
     }
 
-    return msgsToGameManager;
-}
-
-void ConnectionManager::receiveFromGameManager(gameAndUserMsgs& fromGame) {
-    msgsToGameManager.clear();
-    msgsToGameManager.swap(fromGame);
-
-    std::cout<<"in receiveFromGameManager"<<std::endl;
-    for(auto& container : *m_list) {
-        const auto& c = container->getConnection();
-        std::cout<<"current container in m_list: "<<c.id<<std::endl;
-    }
-
-    for (auto& msg : msgsToGameManager) {
-       auto& connection = msg->conn;
-       auto& text = msg->text;
-       auto connContainerItr = std::find_if(m_list->begin(), m_list->end(), find_container(connection));
-       
-        if (connContainerItr == m_list->end()) {
-            auto errMsg = "ConnectionContainer not found while trying to send msg from the GameManager\n";
-            throw std::runtime_error(errMsg);
-        }
-
-        (*connContainerItr)->receiveFromGameManager(text);
-    }
-}
-
-//receive msgs to send from GameManager
-// void rxFromGameManager(std::vector<Interface2Game> msgs);
-
-void ConnectionManager::run() {
-    std::cout << "---------------------MUD Server Console---------------------" << std::endl;
-
-    done = false;
-
-    while (!done) {
-        try {
-            server.update();
-        } catch (std::exception& e) {
-            printf("Exception from Server update:\n%s\n\n", e.what());
-            done = true;
-        }
-
-        auto incoming = server.receive();
-
-        rxFromServer(incoming);
-
-        // auto msgsPtr = send2GameManager();
-
-        // for (const auto& msg : *msgsPtr) {
-        //     std::cout << "GameManager will get msg from: " << msg->conn.id << msg->text << std::endl;
-        // }
-
-        dropConnections();
-
-        sleep(1);
-    }
-}
-
-void ConnectionManager::quit() {
-    done = true;
+    server.send(messages);
 }
 
