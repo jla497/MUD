08821e5d3cf5d92de4ef5231554954c050bd08c0
jla497 <jla497@sfu.ca>
2018-01-27 15:51:03 -0800

    added back test folder

---
+++ MudServer/test/ConnectionManagerTests.cpp
@@ -0,0 +1,115 @@
+#include "ConnectionManager.h"
+#include "gtest/gtest.h"
+namespace {
+// To use a test fixture, derive a class from testing::Test.
+class ConnectionManTest : public testing::Test {
+protected:  // You should make the members protected s.t. they can be
+  // accessed from sub-classes.
+
+  // virtual void SetUp() will be called before each test is run.  You
+  // should define it if you need to initialize the variables.
+  // Otherwise, this can be skipped.
+  virtual void SetUp() {
+    Message m1{Connection{39985500},"msg1"};
+    Message m2{Connection{39985499},"msg2"};
+    Message m3{Connection{39985600},"msg3"};
+    incoming.push_back(m1);
+    incoming.push_back(m2);
+    incoming.push_back(m3);
+    
+    auto reply1 = std::make_unique<gameAndUserInterface>();
+    reply1->conn = Connection{39985500};
+    reply1->text = "reply";
+
+    
+    auto reply2 = std::make_unique<gameAndUserInterface>();
+    reply2->conn = Connection{39985499};
+    reply2->text = "reply";
+
+
+    auto reply3 = std::make_unique<gameAndUserInterface>();
+    reply3->conn = Connection{39985600};
+    reply3->text = "reply";
+
+    gameMsgs.push_back(std::move(reply1));
+    gameMsgs.push_back(std::move(reply2));
+    gameMsgs.push_back(std::move(reply3));
+}
+
+// virtual void TearDown() will be called after each test is run.
+// You should define it if there is cleanup work to do.  Otherwise,
+// you don't have to provide it.
+
+// virtual void TearDown() {
+// }
+
+// Declares the variables your tests want to use.
+ConnectionManager m_manager;
+std::deque<Message> incoming;
+gameAndUserMsgs gameMsgs;
+};
+
+// When you have a test fixture, you define a test using TEST_F
+// instead of TEST.
+
+// TEST_F(ConnectionManTest, AddConnectionContainer) {
+//   ASSERT_NO_THROW(m_manager->addConnection(Connection{400033}));
+
+//   ASSERT_NO_THROW(m_manager->addConnection(Connection{302348}));
+// }
+
+TEST_F(ConnectionManTest, TestProtocolInConnectionContainer) {
+  Connection c{39990000};
+  auto container = std::make_unique<ConnectionContainer>(c);
+  std::string str("");
+  ASSERT_NO_THROW(container->receiveFromServer(str));
+
+  auto cmpStr = container->sendToGameManager();
+  EXPECT_EQ("", cmpStr);
+  // std::cout<<container->getOutBuffer()<<std::endl;
+}
+
+TEST_F(ConnectionManTest, SendFromConnectionManagerToConnectionContainerProtocol) {
+  
+  ASSERT_NO_THROW(m_manager.rxFromServer(incoming));
+}
+
+TEST_F(ConnectionManTest, SendFromConnectionManagerToGameManager) {
+ m_manager.rxFromServer(incoming);
+  auto& msgs = m_manager.send2GameManager();
+
+  auto itr = std::find_if(msgs.begin(), msgs.end(), find_gameAndUserInterface(Connection{39985500}));
+  const auto& text = (*itr)->text;
+  ASSERT_EQ(text,"msg1\n");
+
+  itr = std::find_if(msgs.begin(), msgs.end(), find_gameAndUserInterface(Connection{39985499}));
+  const auto& text2 = (*itr)->text;
+  ASSERT_EQ(text2,"msg2\n");
+
+  itr = std::find_if(msgs.begin(), msgs.end(), find_gameAndUserInterface(Connection{39985600}));
+  const auto& text3 = (*itr)->text;
+  ASSERT_EQ(text3,"msg3\n");
+
+  }
+
+  TEST_F(ConnectionManTest, SendFromGameManagerToServer) {
+    m_manager.rxFromServer(incoming);
+    auto& msgs = m_manager.send2GameManager();
+    ASSERT_NO_THROW(m_manager.receiveFromGameManager(gameMsgs));
+    auto msgsToServer = m_manager.sendToServer();
+
+    for(auto& msg: msgsToServer) {
+      ASSERT_EQ("reply\n",msg.text);
+    }
+
+  }
+
+// Tests Dequeue().
+// TEST_F(ConnectionManTest, SendMessagesToGameManager) {
+//   MsgsPtr messages = m_manager.send2GameManager();
+
+//   const auto& msg = messages->at(0);
+//   // std::cout<<msg->text<<std::endl;
+//    EXPECT_EQ("msg1", "msg1");
+// }
+}  // namespace
