f7a93af8ccb1e48b1bfcb036e2933f9218db8883
binglett <blng@sfu.ca>
2018-02-03 20:14:03 -0800

    added PlayerCharacter

---
+++ MudGameEngine/lib/entities/PlayerCharacterEntity.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+#include "entities/PlayerCharacterEntity.h"
+
+PlayerCharacterEntity::PlayerCharacterEntity(int armor, std::string& damage,
+                     std::vector<std::string>& desc, unsigned int exp, int gold,
+                     std::string& hit, unsigned int npcTypeId,
+                     std::vector<std::string>& keywords, unsigned int level,
+                     std::vector<std::string>& longDesc, std::string& shortDesc,
+                     int thac0)
+
+    : CharacterEntity::CharacterEntity(armor, damage, desc, exp, gold,
+         hit, npcTypeId, keywords, level, longDesc,
+         shortDesc, thac0) {}
