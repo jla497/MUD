b361437c0d108c9ef7f68f0aae40da2afda702fa
amaladwa <amaladwa@sfu.ca>
2018-02-04 23:44:48 -0800

    stub attack

--- MudGameEngine/lib/actions/AttackAction.cpp
+++ MudGameEngine/lib/actions/AttackAction.cpp
@@ -4,20 +4,45 @@
 #include "actions/AttackAction.h"
 
 void AttackAction::execute() {
+	static auto logger = mudserver::logging::getLogger("AttackAction::execute");
+    logger->info("Entered AttackAction");
+
 	//get gamestate
 	auto& gameState = gameManager.getState();
 
-	//get my character
-	// auto myChar = gameState.getCharacter(characterPerformingAction);
+    // get player who is attacking
+    auto playerAttacking = this->characterPerformingAction;
+    auto attackTargetNames = this->entitiesBeingActedUpon;
 
-	// //get my room
-	// auto room = gameState.getCharacterLocation(myChar);
-	// //see if my target is in the same room
-	// auto others = gamestate.getCharactersInRoom(room);
+    //--get the room the player is in
+    auto roomPlayerIsIn = gameState.getCharacterLocation(&playerAttacking);
+    if(roomPlayerIsIn == nullptr){
+        logger->error("roomPlayerIsIn does not exist");
+    }
+
+    //--get list of entities in the room
+    auto IDsOfPlayersInRoom = gameState.getCharactersInRoom(roomPlayerIsIn);
+    auto attackingPlayersId = playerAttacking.getEntityId().getId();
+    auto attackingPlayersShortDesc = playerAttacking.getShortDesc();
+
+	//see if my target is in the same room
+
 
 	// for(const auto id: others) {
 	// //if it is kill and return 
 	// 	//else fail and return
-
 	// }
+
+    //TODO: actually be able to attack entities
+    auto nameOfAttackTarget = attackTargetNames.at(0);
+
+        logger->info("calling gameManager.sendCharacterMessage()");
+            gameManager.sendCharacterMessage(attackingPlayersId,
+                "You try to attack" + nameOfAttackTarget + 
+                "but you can't, the feature is unimplemented");
+    
+
+
+    logger->info("Exiting AttackAction");
+
 }
