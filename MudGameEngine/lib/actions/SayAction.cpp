d07c9cd00b79edb184152994c337d25b7e56a61e
amaladwa <amaladwa@sfu.ca>
2018-02-04 20:00:58 -0800

    Add logging inside SayAction

--- MudGameEngine/lib/actions/SayAction.cpp
+++ MudGameEngine/lib/actions/SayAction.cpp
@@ -12,14 +12,24 @@ void SayAction::execute() {
 
     // get player who is saying the message
     auto playerSayingMessage = playerCharacter;
+    auto playersMessageTokens = entitiesBeingActedUpon;
     if(playerSayingMessage == NULL){
         //output error (DEBUG LOG)
         logger->error("Player does not exist");
     	return;
     }
 
+    //TODO: make ActionParser send the whole string, 
+    //currently we are doing a lot of work right before executing the game logic,
+    //it would be better to do this when the action is created.
+
     // get the message the player wants to send
-    auto messageSentByPlayer = entitiesBeingActedUpon.at(0);
+    std::string messageSentByPlayer;
+    for(auto const& stringToken : playersMessageTokens ){
+        messageSentByPlayer += stringToken;
+    }
+    //auto messageSentByPlayer = playersMessageTokens.at(0);
+
     if(messageSentByPlayer.empty()){
         //do nothing on an empty messsage
         return;
