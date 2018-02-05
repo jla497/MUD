cf7d1250e876a83f7f0ddf320f23baa46ee7f334
amaladwa <amaladwa@sfu.ca>
2018-02-04 23:59:45 -0800

    commented out attackACtion

--- MudGameEngine/lib/actions/AttackAction.cpp
+++ MudGameEngine/lib/actions/AttackAction.cpp
@@ -2,47 +2,48 @@
 #include <vector>
 
 #include "actions/AttackAction.h"
+#include "logging.h"
 
 void AttackAction::execute() {
-	static auto logger = mudserver::logging::getLogger("AttackAction::execute");
-    logger->info("Entered AttackAction");
+	// static auto logger = mudserver::logging::getLogger("AttackAction::execute");
+ //    logger->info("Entered AttackAction");
 
-	//get gamestate
-	auto& gameState = gameManager.getState();
+	// //get gamestate
+	// auto& gameState = gameManager.getState();
 
-    // get player who is attacking
-    auto playerAttacking = this->characterPerformingAction;
-    auto attackTargetNames = this->entitiesBeingActedUpon;
+ //    // get player who is attacking
+ //    auto playerAttacking = this->characterPerformingAction;
+ //    auto attackTargetNames = this->entitiesBeingActedUpon;
 
-    //--get the room the player is in
-    auto roomPlayerIsIn = gameState.getCharacterLocation(&playerAttacking);
-    if(roomPlayerIsIn == nullptr){
-        logger->error("roomPlayerIsIn does not exist");
-    }
+ //    //--get the room the player is in
+ //    auto roomPlayerIsIn = gameState.getCharacterLocation(&playerAttacking);
+ //    if(roomPlayerIsIn == nullptr){
+ //        logger->error("roomPlayerIsIn does not exist");
+ //    }
 
-    //--get list of entities in the room
-    auto IDsOfPlayersInRoom = gameState.getCharactersInRoom(roomPlayerIsIn);
-    auto attackingPlayersId = playerAttacking.getEntityId().getId();
-    auto attackingPlayersShortDesc = playerAttacking.getShortDesc();
+ //    //--get list of entities in the room
+ //    auto IDsOfPlayersInRoom = gameState.getCharactersInRoom(roomPlayerIsIn);
+ //    auto attackingPlayersUniqueId = playerAttacking.getEntityId();
+ //    auto attackingPlayersShortDesc = playerAttacking.getShortDesc();
 
-	//see if my target is in the same room
+	// //see if my target is in the same room
 
 
-	// for(const auto id: others) {
-	// //if it is kill and return 
-	// 	//else fail and return
-	// }
+	// // for(const auto id: others) {
+	// // //if it is kill and return 
+	// // 	//else fail and return
+	// // }
 
-    //TODO: actually be able to attack entities
-    auto nameOfAttackTarget = attackTargetNames.at(0);
+ //    //TODO: actually be able to attack entities
+ //    auto nameOfAttackTarget = attackTargetNames.at(0);
 
-        logger->info("calling gameManager.sendCharacterMessage()");
-            gameManager.sendCharacterMessage(attackingPlayersId,
-                "You try to attack" + nameOfAttackTarget + 
-                "but you can't, the feature is unimplemented");
+ //        logger->info("calling gameManager.sendCharacterMessage()");
+ //            gameManager.sendCharacterMessage(attackingPlayersUniqueId,
+ //                "You try to attack" + nameOfAttackTarget + 
+ //                "but you can't, the feature is unimplemented");
     
 
 
-    logger->info("Exiting AttackAction");
+ //    logger->info("Exiting AttackAction");
 
 }
