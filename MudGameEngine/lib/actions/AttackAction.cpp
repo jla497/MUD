99f72e1ea9e5e517de21c99ede15b8a1710c2cd3
amaladwa <amaladwa@sfu.ca>
2018-02-05 21:26:12 -0800

    Implement stub PVP attacking

--- MudGameEngine/lib/actions/AttackAction.cpp
+++ MudGameEngine/lib/actions/AttackAction.cpp
@@ -1,49 +1,81 @@
 #include <string>
 #include <vector>
+#include <boost/algorithm/string.hpp>
 
 #include "actions/AttackAction.h"
 #include "logging.h"
+#include "gamemanager/GameManager.h"
+
 
 void AttackAction::execute() {
-	// static auto logger = mudserver::logging::getLogger("AttackAction::execute");
- //    logger->info("Entered AttackAction");
+	static auto logger = mudserver::logging::getLogger("AttackAction::execute");
+    logger->info("Entered AttackAction");
 
-	// //get gamestate
-	// auto& gameState = gameManager.getState();
+	//get gamestate
+	auto& gameState = gameManager.getState();
 
- //    // get player who is attacking
- //    auto playerAttacking = this->characterPerformingAction;
- //    auto attackTargetNames = this->actionArguments;
+	// get player who is attacking
+    auto playerWhoIsAttacking = characterPerformingAction;
 
- //    //--get the room the player is in
- //    auto roomPlayerIsIn = gameState.getCharacterLocation(&playerAttacking);
- //    if(roomPlayerIsIn == nullptr){
- //        logger->error("roomPlayerIsIn does not exist");
- //    }
+    //--get the room the player is in
+    auto characterCurrentRoom =
+        gameState.getCharacterLocation(&playerWhoIsAttacking);
+    if (!characterCurrentRoom) {
+        logger->error(
+            "Character is not in a room! Suspect incorrect world init");
+        // return early, as we are in a bad state - the player is not in a room!
+        return;
+    }
 
- //    //--get list of entities in the room
- //    auto IDsOfPlayersInRoom = gameState.getCharactersInRoom(roomPlayerIsIn);
- //    auto attackingPlayersUniqueId = playerAttacking.getEntityId();
- //    auto attackingPlayersShortDesc = playerAttacking.getShortDesc();
-
-	// //see if my target is in the same room
-
-
-	// // for(const auto id: others) {
-	// // //if it is kill and return 
-	// // 	//else fail and return
-	// // }
-
- //    //TODO: actually be able to attack entities
- //    auto nameOfAttackTarget = attackTargetNames.at(0);
-
- //        logger->info("calling gameManager.sendCharacterMessage()");
- //            gameManager.sendCharacterMessage(attackingPlayersUniqueId,
- //                "You try to attack" + nameOfAttackTarget + 
- //                "but you can't, the feature is unimplemented");
-    
+    //--get list of entities in the room
+    auto IDsOfPlayersInRoom = gameState.getCharactersInRoom(characterCurrentRoom);
+    if(IDsOfPlayersInRoom.empty()){
+    	return;
+    }
+    auto attackingPlayersUniqueId = playerWhoIsAttacking.getEntityId();
+    if(actionArguments.empty()){
+    	//user did not pass an attack target
+    	gameManager.sendCharacterMessage(attackingPlayersUniqueId,
+    	"Attack what?");
+    	logger->info("No Target found");
+    	return;
+    }
+    auto nameOfAttackTarget = actionArguments.at(0);
+   	logger->info("Name of attacker: " + nameOfAttackTarget);
 
 
- //    logger->info("Exiting AttackAction");
+    //TODO: make changes so that the player can attack any arbritrary entity.
+	//see if my target is in the same room
+    for (auto characterID : IDsOfPlayersInRoom) {
+    	auto currentEntity = gameState.getCharacterFromLUT(characterID);
+    	if(!currentEntity)
+    		return;
+    	//auto keyWordsOfCurrentEntity = currentEntity->getKeywords();
+    	auto shortDescOfCurrentPlayer = currentEntity->getShortDesc();
+    	//for(auto keyword : keyWordsOfCurrentEntity){
+    		//if(boost::to_upper_copy(keyword).compare(boost::to_upper_copy(nameOfAttackTarget)) == 0){
+    		if(boost::to_upper_copy(shortDescOfCurrentPlayer).compare(boost::to_upper_copy(nameOfAttackTarget)) == 0){
+    			//we found the entity the player wants to fight.
+    			//TODO: change this to allow attacking any entity
+    			//startPVPAttack(playerWhoIsAttacking,currentEntity,gameManager);
 
+    			//TODO: implement proper use of combat states
+				//TODO: implement proper combat(in a seperate class)
+				//send messages to characters fighting
+				auto playerWhoIsBeingAttacking = currentEntity;
+				gameManager.sendCharacterMessage(playerWhoIsAttacking.getEntityId(),
+			       "You attack" + playerWhoIsBeingAttacking->getShortDesc() + "and do 0.00000000000001 damage");
+
+				gameManager.sendCharacterMessage(playerWhoIsBeingAttacking->getEntityId(),
+			       "You are attacked by " + playerWhoIsAttacking.getShortDesc() + "and take 0.00000000000001 damage");
+    			return;
+    		}
+    	}
+   // }
+  
+	//if we didnt find the target we tell the player
+	logger->info("No Target found");
+    gameManager.sendCharacterMessage(attackingPlayersUniqueId,
+    	"Attack failed: could not find " + nameOfAttackTarget);
+    logger->info("Exiting AttackAction");
 }
