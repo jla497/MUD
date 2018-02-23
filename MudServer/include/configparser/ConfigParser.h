a7d64b7a9265c9031a1a8c0a9c16791a3b58d788
jla497 <jla497@sfu.ca>
2018-02-22 17:56:26 -0800

    fix configparser

--- MudServer/include/configparser/ConfigParser.h
+++ MudServer/include/configparser/ConfigParser.h
@@ -8,7 +8,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <yaml-cpp/yaml.h>
+#include "yaml-cpp/yaml.h"
+
 
 using networking::Port;
 
@@ -19,31 +20,6 @@ struct ConfigData {
     std::string url;
 };
 
-boost::optional<ConfigData> parseConfigFile(const std::string &filePath) {
-    std::vector<YAML::Node> data;
-
-    try {
-        data = YAML::LoadAllFromFile(filePath);
-
-    } catch (const std::exception &e) {
-        std::cout << "something is wrong with the config file path or the file "
-                     "doesn't exist...";
-        // logger print error
-        return boost::none;
-    }
-
-    // auto portNum = data[0]["SERVER"]["port"].as<unsigned int>();
-    // std::cout<<"filepath:"<<data[0]["SERVER"]["yml_file"].as<std::string>();
-
-    // Port port{portNum};
-
-    ConfigData serverData{
-        Port{data[0]["SERVER"]["port"].as<short unsigned int>()},
-        data[0]["CLIENT"]["port"].as<std::string>(),
-        data[0]["SERVER"]["yml_file"].as<std::string>(),
-        data[0]["CLIENT"]["url"].as<std::string>()};
-
-    return serverData;
-};
+boost::optional<ConfigData> parseConfigFile(const std::string filePath);
 
 #endif
