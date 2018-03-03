55ed9df75b690119232751f2a577dd48da459f87
De Wet van Niekerk <dewert@gmail.com>
2018-03-03 10:16:45 -0800

    Fixed tests, clang format

--- MudServer/test/include/MockGameManager.h
+++ MudServer/test/include/MockGameManager.h
@@ -1,10 +1,12 @@
 #include "connectionmanager/ConnectionManager.h"
 #include "gamemanager/GameManager.h"
+#include "persistence/PersistenceService.h"
 #include "gmock/gmock.h"
 
 class MockGameManager : public mudserver::gamemanager::GameManager {
-  public:
+    mudserver::persistence::PersistenceService ps;
+public:
     MockGameManager(mudserver::connection::ConnectionManager &cm,
                     mudserver::gamemanager::GameState &gs)
-        : GameManager(cm, gs) {}
+        : ps{""}, GameManager(cm, gs, ps) {}
 };
