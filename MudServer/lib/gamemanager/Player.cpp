7bd355de1d2c50862c11f426a13c27fd186136ad
Yuhong Bao <yuhongb@sfu.ca>
2018-03-15 20:44:10 -0700

    add code to Player.cpp for SHA256 hash

--- MudServer/lib/gamemanager/Player.cpp
+++ MudServer/lib/gamemanager/Player.cpp
@@ -1,6 +1,8 @@
 #include "gamemanager/Player.h"
 #include <Server.h>
-
+extern "C" {
+#include "gamemanager/sha256.h"
+}
 namespace mudserver {
 namespace gamemanager {
 
@@ -22,5 +24,30 @@ void Player::setConnectionId(networking::ConnectionId id) { connectionId = id; }
 
 bool Player::hasAdminPrivilege() { return isAdmin; }
 void Player::getAdminPrivilege() { isAdmin = true; }
+void hashtostr(char *hash, char * str, int length)
+{
+	int i = 0,j = 0;
+	const char letters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	for (i=0; i<length; i++)
+	{
+		unsigned char val = hash[i];
+		str[j+1] = letters[val & 0xf];
+		str[j] = letters[val >> 4];
+		j += 2;
+	}
+	str[j] = 0;
+}
+std::string sha256str(std::string str)
+{
+	SHA256_CTX ctx;
+	unsigned char hash[32];
+	char str2[129];	
+	sha256_init(&ctx);
+	sha256_update(&ctx, (unsigned char*)str.c_str(), str.length());
+	sha256_final(&ctx, hash);
+	hashtostr((char*)hash, (char*)str2,32);
+	return std::string((char*)str2);
+}
+
 } // namespace gamemanager
 } // namespace mudserver
