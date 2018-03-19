27d110bdfdefe6f4b1df0f667f313081365606f7
binglett <blng@sfu.ca>
2018-03-18 17:33:23 -0700

    take action can take multi-word args

--- MudGameEngine/include/actions/TakeAction.h
+++ MudGameEngine/include/actions/TakeAction.h
@@ -12,10 +12,12 @@ class TakeAction : public Action {
   public:
   	using Action::Action;
   	void execute_impl() override;
+  	std::unique_ptr<Action> clone() const override; 
 
   protected:
   	std::string description() const override { return u8"Take action"; }
-  	std::unique_ptr<TakeAction> clone() override; // need to change this...
+  	const unsigned int MAX_TAKE_ARGS = 1; // so far take one object at a time
+
 };
 
 #endif // TAKEACTION_H
--- MudGameEngine/lib/actions/TakeAction.cpp
+++ MudGameEngine/lib/actions/TakeAction.cpp
@@ -1,11 +1,57 @@
 #include "actions/TakeAction.h"
+#include "entities/RoomEntity.h"
 #include "logging.h"
 
-std::unique_ptr<TakeAction> TakeAction::clone() { return std::unique_ptr<TakeAction>(*this); }
+#include <boost/optional.hpp>
+#include <boost/algorithm/string/join.hpp>
+
+using boost::algorithm::join;
+
+std::unique_ptr<Action> TakeAction::clone() const { 
+	return std::make_unique<TakeAction>(*this); }
 // TakeAction *TakeAction::clone() { return new TakeAction(*this); }
 
 void TakeAction::execute_impl() {
 	static auto logger =
 		mudserver::logging::getLogger("Action::TakeAction");
+
+	auto &gameState = gameManager.getState();
+
+	auto characterCurrentRoom =
+        gameState.getCharacterLocation(*characterPerformingAction);
+    if (!characterCurrentRoom) {
+        logger->error(
+            "Character is not in a room! Suspect incorrect world init");
+        // return early, as we are in a bad state - the player is not in a room!
+        return;
+    }
+
+    if (!actionArguments.empty()) {
+    	auto nameOfObject = join(actionArguments, " ");    	
+    	logger->debug("Trying to move " + nameOfObject + " to inventory");
+
+    	// // get object from room by name 
+    	// // TODO: method to take object by name
+    	// boost::optional<ObjectEntity> objectToTake = nullptr;
+    	// // if object is null, return error msg to players
+    	// if (objectToTake) {
+	    // 	// add to character's onjects
+	    // 	// characterPerformingAction.addObject(objectToTake);    		
+	    // 	gameManager.sendCharacterMessage<(
+    	// 		characterPerformingAction->getEntityId(),
+    	// 		"Object " + nameOfObject + " found in room");
+    	// } else {
+    	// 	gameManager.sendCharacterMessage<(
+    	// 		characterPerformingAction->getEntityId(),
+    	// 		"Object " + nameOfObject + " not found in room");
+    		 
+    	// }
+
+    } else {
+    	// error that you need to try and take something
+    	gameManager.sendCharacterMessage(
+            characterPerformingAction->getEntityId(),
+            "Please type /'take/' <object>/'");
+    } 
 
 }
