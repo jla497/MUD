acf22de4bea6e87d517928c3c1716d472406cfae
Yuhong Bao <yuhongb@sfu.ca>
2018-03-26 18:49:30 -0700

    fixed passwordEquals

--- MudServer/lib/gamemanager/Player.cpp
+++ MudServer/lib/gamemanager/Player.cpp
@@ -14,7 +14,7 @@ PlayerId Player::getId() const { return id; }
 UsernameType Player::getUsername() const { return username; }
 
 bool Player::passwordEquals(const PasswordType &password) const {
-    return password == this->password;
+    return sha256str(password) == this->password;
 }
 
 networking::ConnectionId Player::getConnectionId() const {
